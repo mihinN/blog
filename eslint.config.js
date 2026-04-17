2c25c446684db7ce02ff5f0fa09814d8e84ff6be
melkorevshere <39882449+mihinN@users.noreply.github.com>
2026-04-17 21:23:05 +0530

    updated

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import globals from "globals";
+
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "script",
+      globals: {
+        ...globals.browser,
+      },
+    },
+    rules: {
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "no-undef": "error",
+      "no-implicit-globals": "error",
+      "no-eval": "error",
+      "no-implied-eval": "error",
+      "no-new-func": "error",
+      "no-script-url": "error",
+      "no-var": "warn",
+      "prefer-const": "warn",
+    },
+  },
+];
